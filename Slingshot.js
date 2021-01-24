aaaec094a7247fd991bbe1ae10c8a3a36c78c0ea
ishaaan325jain <swati.912.jain@gmail.com>
2021-01-23 22:09:24 -0800

    slingshot changes

---
+++ Slingshot.js
@@ -0,0 +1,26 @@
+class Slingshot{
+    constructor(bodyA, point){
+        var options = {
+            bodyA: bodyA,
+            pointB: point,
+            stiffness: 1,
+            length: 30
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+         }
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+}
